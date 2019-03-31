d47aee8dd44e416fe8a049b324cdea43360dba1c
GentlemanNero <48154815+GentlemanNero@users.noreply.github.com>
2019-03-31 14:53:52 +0800

    python第一次作业PB17061135刘家宇
    使用和风天气API对合肥进行天气预报，用GUI呈现，用户可以输入想要进行的活动交由程序判断当天是否适宜。

--- PythonHomework/classes.py
+++ PythonHomework/classes.py
@@ -8,11 +8,18 @@ class WeatherAnalysis(object):
         self.day2 = day2
     def average_tmp(self):
         #三天平均温度计算
-        self.average_tmp = ((float(self.day0['tmp_max'])+float(self.day0['tmp_min']))/2+(float(self.day1['tmp_max'])+float(self.day1['tmp_min']))/2+(float(self.day2['tmp_max'])+float(self.day2['tmp_min']))/2)/3
+        self.average_tmp = ( (float(self.day0['tmp_max'])+float(self.day0['tmp_min']))/2+
+                             (float(self.day1['tmp_max'])+float(self.day1['tmp_min']))/2+
+                             (float(self.day2['tmp_max'])+float(self.day2['tmp_min']))/2) /3
         return self.average_tmp
     def tmp_difference(self):
         #三天温差计算
-        self.tmp_difference = max(float(self.day0['tmp_max']),float(self.day1['tmp_max']),float(self.day2['tmp_max']))-min(float(self.day0['tmp_min']),float(self.day1['tmp_min']),float(self.day2['tmp_min']))
+        self.tmp_difference = max(float(self.day0['tmp_max']),
+                                  float(self.day1['tmp_max']),
+                                  float(self.day2['tmp_max']))-
+                              min(float(self.day0['tmp_min']),
+                                  float(self.day1['tmp_min']),
+                                  float(self.day2['tmp_min']))
         return self.tmp_difference
 
 
